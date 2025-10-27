2e294406414f388a67a746cc3053da000c885602
Rutayisire Lucien <rutayisirelucien@gmail.com>
2025-10-27 17:31:01 -0400

    Initial Commit

---
+++ Sona/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sona
+//
+//  Created by user278698 on 10/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sona/SonaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonaApp.swift
+//  Sona
+//
+//  Created by user278698 on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct SonaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
